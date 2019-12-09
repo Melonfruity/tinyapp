bd77ecbf14a1ea5d60da86012ddab7ff9abc4a53
melonfruity <eric.luo24@gmail.com>
2019-12-09 15:31:19 -0500

    added a random 6 alphanumeric string generator

---
+++ express_server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const PORT = 8000;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString() {
+  let randString = [];
+  for (let i = 0; i < 6; i ++) {
+    let charOrNum = Math.round(Math.random());
+    let randomChar = String.fromCharCode(Math.round(Math.random()) ? Math.round(Math.random() * 26) + 65 : Math.round(Math.random() * 26) + 97);
+    let randomNum = Math.round(Math.random() * 9);
+    charOrNum ? randString.push(randomChar) : randString.push(randomNum); 
+  }
+  return randString.join('')
+}
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/urls', (req, res) => {
+  let templateVariables = {
+    urls: urlDatabase
+  }
+  res.render('urls_index', templateVariables);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVariables = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  }
+  res.render(`urls_show`, templateVariables);
+})
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send();
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
+
+// curl -i http://localhost:8000/hello
